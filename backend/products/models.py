2c0f02b9ebbcf72b4dcea30acb84a98fcec6870a
nurilloh2004 <nurillohpython@gmail.com>
2022-10-23 12:20:36 +0500

    123

--- backend/products/models.py
+++ backend/products/models.py
@@ -9,4 +9,3 @@ class Product(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
-    
